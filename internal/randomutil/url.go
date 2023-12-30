9d53e0fc1d3e89f8152596f99d781dd493df879f
BiBi <45527552+bibi-ic@users.noreply.github.com>
2023-12-30 14:54:05 +0700

    happy case for retrieve controller (#9)

--- internal/randomutil/url.go
+++ internal/randomutil/url.go
@@ -5,10 +5,18 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"path/filepath"
 )
 
 func RandURL() string {
-	f, err := os.Open("testURL.data")
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal("cannot get working dir: ", err)
+	}
+	parent := filepath.Dir(path)
+	root := filepath.Dir(parent)
+
+	f, err := os.Open(root + "/test/testdata/testURL.data")
 	if err != nil {
 		log.Fatal("cannot get random url: ", err)
 	}
